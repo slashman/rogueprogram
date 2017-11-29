1d7a7e11b7fb6755796f30ab04092ea2c3df9cb5
Santiago Zapata <slash@slashware.net>
2017-11-29 12:42:01 -0500

    Movies catalog map

--- src/js/levels/rentMovie.js
+++ src/js/levels/rentMovie.js
@@ -1,62 +1,31 @@
 module.exports = {
 	baseMap: [
-		   //01234567890123456789012345678901234567890123456789012345678901234567890123456789
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                    RENT MOVIE                                                  ",
-		"                                                                                ",
-		"                                                                                ",
-		"                        %                                                       ",
-		"                                                                                ",
-		"                                                                                ",
-		"                    %                                                           ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                ",
-		"                                                                                "
+"  ╔══════════════════════════════════════════════════════════════════════════╗  ",
+"  ║                                                                          ║  ",
+"  ║                  MOVIES AND SERIES CATALOG                               ║  ",
+"  ║                                                                          ║  ",
+"  ╚══════════════════════════════════════════════════════════════════════════╝  ",
+"                                                                                ",
+"   Search:                                                                      ",
+"  ┌──────────────────────────────────────────────────────────────────────────┐  ",
+"  │Title                  Type        Cat              Sta          Cust     │  ",
+"  │Robotech               Series      Japanimation     AVAILABLE             │  ",
+"  │G.I. Joe               Series      Cartoon          RENTED       MTOY     │  ",
+"  │Thundercats            Series      Cartoon          RENTED       GWICH    │  ",
+"  │Star Trek              Series      Science Fiction  AVAILABLE             │  ",
+"  │Star Wars              Movie       Science Fiction  RENTED       MADMAX   │  ",
+"  │Karate Kid             Movie       Action           AVAILABLE             │  ",
+"  │Back to the Future     Movie       Science Fiction  RENTED       SLASH    │  ",
+"  │Indiana Jones 2        Movie       Action           AVAILABLE             │  ",
+"  └──────────────────────────────────────────────────────────────────────────┘  ",
+" ┌───────────────────┐                            %                             ",
+" │* Back to Main Menu│        %                                                 ",
+" └───────────────────┘                                                          ",
+"                                                                                ",
 	],
-	exits: [
-		{
-			to: "mainMenu",
-			on: {
-				x: 35,
-				y: 21
-			}
-		}
-	],
+	exits: [{to: "mainMenu"}],
 	hints: [
 		"The only way to access the games menu now is using the Primary Keys.",
 		"This system replaced the index cards to control VHS tape rentals"
 	]
 }
-
-/*
-
-TV Shows
- Japanimation
-  Robotech
-  Voltron
- Cartoons
-  The Transformers
-  G.I. Joe
-  SilverHawks
-  Thunder Cats
- Sci Fi
-  Star Trek
-
-Movies
- Star Wars
- Karate Kid
- Back to the Future
- Indiana Jones
-
- */
